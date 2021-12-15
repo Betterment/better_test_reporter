b91a50f838134b01f9cbdd4585d6e83ac4df6bf8
Caleb Donoho <caleb.donoho@betterment.com>
2021-12-15 18:16:48 -0500

    go back go back go back

--- scripts/generate_code_coverage.sh
+++ scripts/generate_code_coverage.sh
@@ -12,4 +12,5 @@ dart pub global activate coverage
 dart test --coverage=coverage && dart pub global run coverage:format_coverage --lcov --in=coverage --out=coverage/lcov.info --packages=.packages --report-on=lib
 
 # Remove coverage report for generated files
+dart pub global run remove_from_coverage:remove_from_coverage -f coverage/lcov.info -r '.freezed.dart$'
 dart pub global run remove_from_coverage:remove_from_coverage -f coverage/lcov.info -r '.g.dart$'
