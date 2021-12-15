2767dff7b28db6b8963357edf56b031146005e1e
Caleb Donoho <caleb.donoho@betterment.com>
2021-12-15 18:13:46 -0500

    Remove removing freezed from coverage, as they should be ignored now

--- scripts/generate_code_coverage.sh
+++ scripts/generate_code_coverage.sh
@@ -12,5 +12,4 @@ dart pub global activate coverage
 dart test --coverage=coverage && dart pub global run coverage:format_coverage --lcov --in=coverage --out=coverage/lcov.info --packages=.packages --report-on=lib
 
 # Remove coverage report for generated files
-dart pub global run remove_from_coverage:remove_from_coverage -f coverage/lcov.info -r '.freezed.dart$'
 dart pub global run remove_from_coverage:remove_from_coverage -f coverage/lcov.info -r '.g.dart$'
